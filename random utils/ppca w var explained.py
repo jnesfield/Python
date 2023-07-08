14efcea507dd5b858e0334f34cd9b3979da51185
jnesfield <jamesnesfield@live.com>
2023-07-08 06:48:46 -0700

    Add files via upload

---
+++ random utils/ppca w var explained.py
@@ -0,0 +1,113 @@
+import numpy as np
+from numpy import shape, isnan, nanmean, average, log, cov
+from numpy.matlib import repmat
+from numpy.random import normal
+from numpy.linalg import inv, det, eig
+from numpy import identity as eye
+from numpy import trace as tr
+from scipy.linalg import orth
+
+
+def ppca(Y, d, dia):
+    """
+    Implements probabilistic PCA for data with missing values,
+    using a factorizing distribution over hidden states and hidden observations.
+
+    Args:
+        Y:   (N by D ) input numpy ndarray of data vectors
+        d:   (  int  ) dimension of latent space
+        dia: (boolean) if True: print objective each step
+
+    Returns:
+        C:  (D by d ) C*C' + I*ss is covariance model, C has scaled principal directions as cols
+        ss: ( float ) isotropic variance outside subspace
+        M:  (D by 1 ) data mean
+        X:  (N by d ) expected states
+        Ye: (N by D ) expected complete observations (differs from Y if data is missing)
+
+        Based on MATLAB code from J.J. VerBeek, 2006. http://lear.inrialpes.fr/~verbeek
+    """
+    N, D = shape(Y)  # N observations in D dimensions (i.e. D is number of features, N is samples)
+    threshold = 1E-4  # minimal relative change in objective function to continue
+    hidden = isnan(Y)
+    missing = hidden.sum()
+
+    if missing > 0:
+        M = nanmean(Y, axis=0)
+    else:
+        M = average(Y, axis=0)
+
+    Ye = Y - repmat(M, N, 1)
+
+    if missing > 0:
+        Ye[hidden] = 0
+
+    # initialize
+    C = normal(loc=0.0, scale=1.0, size=(D, d))
+    CtC = C.T @ C
+    X = Ye @ C @ inv(CtC)
+    recon = X @ C.T
+    recon[hidden] = 0
+    ss = np.sum((recon - Ye) ** 2) / (N * D - missing)
+
+    count = 1
+    old = np.inf
+
+    # EM Iterations
+    while (count):
+        Sx = inv(eye(d) + CtC / ss)  # E-step, covariances
+        ss_old = ss
+        if missing > 0:
+            proj = X @ C.T
+            Ye[hidden] = proj[hidden]
+
+        X = Ye @ C @ Sx / ss  # E-step: expected values
+
+        SumXtX = X.T @ X  # M-step
+        C = Ye.T @ X @ (SumXtX + N * Sx).T @ inv(((SumXtX + N * Sx) @ (SumXtX + N * Sx).T))
+        CtC = C.T @ C
+        ss = (np.sum((X @ C.T - Ye) ** 2) + N * np.sum(CtC * Sx) + missing * ss_old) / (N * D)
+        # transform Sx determinant into numpy longdouble in order to deal with high dimensionality
+        Sx_det = np.min(Sx).astype(np.longdouble) ** shape(Sx)[0] * det(Sx / np.min(Sx))
+        objective = N * D + N * (D * log(ss) + tr(Sx) - log(Sx_det)) + tr(SumXtX) - missing * log(ss_old)
+
+        rel_ch = np.abs(1 - objective / old)
+        old = objective
+
+        count = count + 1
+        if rel_ch < threshold and count > 5:
+            count = 0
+        if dia:
+            print(f"Objective: {objective:.2f}, Relative Change {rel_ch:.5f}")
+
+    C = orth(C)
+    covM = cov((Ye @ C).T)
+    vals, vecs = eig(covM)
+    ordr = np.argsort(vals)[::-1]
+    vecs = vecs[:, ordr]
+
+    C = C @ vecs
+    X = Ye @ C
+
+    # add data mean to expected complete data
+    Ye = Ye + repmat(M, N, 1)
+
+    # Compute variance explained by each eigenvector
+    eig_vals = np.sort(vals)[::-1][:d]
+    total_var = np.sum(eig_vals)
+    var_explained = eig_vals / total_var
+
+    # Print variance explained for each eigenvector
+    for i in range(d):
+        print(f"Variance explained by eigenvector {i + 1}: {var_explained[i]}")
+
+    return C, ss, M, X, Ye
+
+    """
+    In this modified code, after the computation of vecs (eigenvectors) and vals (eigenvalues), the code sorts the eigenvalues in descending order and selects the top d eigenvalues. Then, it computes the total variance explained by summing the selected eigenvalues and calculates the variance explained by each eigenvector as a ratio of its eigenvalue to the total variance.
+
+The code then prints the variance explained for each eigenvector using a loop.
+
+Please note that this code assumes you have the necessary dependencies (NumPy, SciPy) installed to run it.
+
+    """
